d30c93f19f573ffe0c8d5e00b1d0491654dae4b6
Raffaello D. Di Napoli <raffaello.dinapoli@gmail.com>
2013-09-17 19:52:00 -0400

    Change ExternalModuleEnumerator to use yield instead of sys.stdout.write
    Add to ExternalModuleEnumerator two iterator methods that replace the
    now-gone enum() method. This allows to re-use the class without invoking a
    child process (hint hint Generator).

--- kerneltools/ExternalModuleEnumerator.py
+++ kerneltools/ExternalModuleEnumerator.py
@@ -33,100 +33,89 @@ import sys
 class ExternalModuleEnumerator(object):
 	"""Enumerates kernel external modules."""
 
-	def __init__(self, bFiles, bFirmware, bModules, bPackages):
+	def __init__(self, bFirmware, bModules):
 		"""Constructor.
 
-		bool bFiles
-			Show matching files for each enumerated item.
 		bool bFirmware
 			Enumerate external firmware installed by non-kernel packages.
 		bool bModules
 			Enumerate modules installed by non-kernel packages.
-		bool bPackages
-			If bFiles is False, only show package names; if bFiles is True, show one matching package
-			per line, with its relevant contents following the package name, on the same line
 		"""
 
-		self._m_bFiles = bFiles
-		self._m_bFirmware = bFirmware
-		self._m_bModules = bModules
-		self._m_bPackages = bPackages
-
 		self._m_reContentsLine = re.compile(r'^obj\s+(?P<path>\S+)\s+')
-		self._m_cchRoot = len(os.environ.get('ROOT', '/'))
+		self._m_bFirmware = bFirmware
 		self._m_sFirmwarePath = 'lib/firmware/'
-
+		self._m_bModules = bModules
+		self._m_cchRoot = len(os.environ.get('ROOT', '/'))
 		self._m_sVdbPath = subprocess.check_output(
 			['portageq', 'vdb_path'],
 			stderr = sys.stderr, universal_newlines = True
 		).rstrip()
 
 
-	def enum(self):
-		"""Enumerates the packages and/or files matching the criteria specified in the constructor."""
+	def files(self):
+		"""Enumerates all files matching the criteria specified in the constructor.
+
+		str yield
+			Path to the matching file.
+		"""
+
+		for sPackage, listFiles in self.packages_and_files(bUseSlot = False):
+			for sFilePath in listFiles:
+				yield sFilePath
+
+
+	def packages_and_files(self, bUseSlot = True):
+		"""Enumerates all packages and/or files matching the criteria specified in the constructor.
+
+		[bool bUseSlot]
+			If True (default), each package will end in its slot number instead of its version.
+		tuple(str, list(str)) yield
+			A tuple containing the package and the matching files it contains.
+		"""
 
 		# List all directories (package categories) in the VDB.
 		for sCategory in os.listdir(self._m_sVdbPath):
 			sCategoryPath = os.path.join(self._m_sVdbPath, sCategory)
-			if os.path.isdir(sCategoryPath):
-				# List all directories (package names) in the category.
-				for sPackageName in os.listdir(sCategoryPath):
-					sPackagePath = os.path.join(sCategoryPath, sPackageName)
-					if os.path.isdir(sPackagePath):
-						# Process this package.
-						self.process_package(sCategory + '/' + sPackageName, sPackagePath)
-
-
-	def process_package(self, sPackage, sPackagePath):
-		"""Examines the contents of a package, looking for files matching the criteria specified in
-		the constructor.
-
-		str sPackage
-			Package category/name.
-		str sPackagePath
-			Path to the package in the VDB.
-		"""
-
-		# Analyze the contents of the package, building a list of files of our interest.
-		listFiles = list()
-		with open(os.path.join(sPackagePath, 'CONTENTS'), 'r') as fileContents:
-			for sLine in fileContents:
-				# Parse the line.
-				match = self._m_reContentsLine.match(sLine)
-				if not match:
-					# Not a file (“obj”).
+			if not os.path.isdir(sCategoryPath):
+				continue
+			# List all directories (package names) in the category.
+			for sPackage in os.listdir(sCategoryPath):
+				sPackagePath = os.path.join(sCategoryPath, sPackage)
+				if not os.path.isdir(sPackagePath):
 					continue
-				# Remove the root.
-				sFilePath = match.group('path')[self._m_cchRoot:]
-				if self._m_bModules and sFilePath.endswith('.ko'):
-					# Remove “lib/modules/linux-*/”.
-					sFilePath = re.sub(r'^lib/modules/[^/]+/', '', sFilePath)
-				elif self._m_bFirmware and sFilePath.startswith(self._m_sFirmwarePath):
-					# Remove “lib/firmware/”.
-					sFilePath = sFilePath[len(self._m_sFirmwarePath):]
-				else:
-					# Not a file we’re interested in.
-					continue
-				# Add this file to the list.
-				listFiles.append(sFilePath)
+				sPackage = sCategory + '/' + sPackage
 
-		if listFiles:
-			# Replace the package version with its slot.
-			if self._m_bPackages:
-				# Get the package slot.
-				with open(os.path.join(sPackagePath, 'SLOT'), 'r') as fileSlot:
-					sPackageSlot = fileSlot.read().strip()
-				sPackage = re.sub(r'-[0-9].*$', ':' + sPackageSlot, sPackage)
-			if self._m_bPackages:
-				if self._m_bFiles:
-					# Output the package and its files.
-					sys.stdout.write(sPackage + ' ' + ' '.join(listFiles) + '\n')
-				else:
-					# Output packages only.
-					sys.stdout.write(sPackage + '\n')
-			elif self._m_bFiles:
-				# Output files only, one per line.
-				sys.stdout.write('\n'.join(listFiles) + '\n')
+				# Analyze the contents of the package, building a list of files of our interest.
+				listFiles = []
+				with open(os.path.join(sPackagePath, 'CONTENTS'), 'r') as fileContents:
+					for sLine in fileContents:
+						# Parse the line.
+						match = self._m_reContentsLine.match(sLine)
+						if not match:
+							# Not a file (“obj”).
+							continue
+						# Remove the root.
+						sFilePath = match.group('path')[self._m_cchRoot:]
+						if self._m_bModules and sFilePath.endswith('.ko'):
+							# Remove “lib/modules/linux-*/”.
+							sFilePath = re.sub(r'^lib/modules/[^/]+/', '', sFilePath)
+						elif self._m_bFirmware and sFilePath.startswith(self._m_sFirmwarePath):
+							# Remove “lib/firmware/”.
+							sFilePath = sFilePath[len(self._m_sFirmwarePath):]
+						else:
+							# Not a file we’re interested in.
+							continue
+						# Add this file to the list.
+						listFiles.append(sFilePath)
+
+				if listFiles:
+					if bUseSlot:
+						# Replace the package version with its slot.
+						with open(os.path.join(sPackagePath, 'SLOT'), 'r') as fileSlot:
+							sPackageSlot = fileSlot.read().strip()
+						sPackage = re.sub(r'-[0-9].*$', ':' + sPackageSlot, sPackage)
+					yield sPackage, listFiles
 
 
 
